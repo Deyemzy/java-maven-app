c37f5d15305916dc0f718e390b8483ebca96e5dd
Deyemzy <adedejiadeyemid@gmail.com>
2022-11-08 22:58:58 -0600

    call library function in jenkinsfile

--- script.groovy
+++ script.groovy
@@ -1,16 +1,3 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t deyemzy/my-repo:jma-1.2 .'
-        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push deyemzy/my-repo:jma-1.2'
-    }
-} 
 
 def deployApp() {
     echo 'deploying the application...'
