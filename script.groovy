7026ede10b2b4d3ba415fa5403130a14a9f73f72
Deyemzy <56632471+Deyemzy@users.noreply.github.com>
2022-11-08 18:16:54 -0600

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         sh 'docker build -t deyemzy/my-repo:jma-1.2 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push deyemzy/my-repo:jma-1.2'
